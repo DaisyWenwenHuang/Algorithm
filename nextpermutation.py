eb240fd3fa1cdac2e474bf63b181486b70ef380e
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-04-26 21:43:01 +0000

    update

---
+++ nextpermutation.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/next-permutation/
+# find the next permutation that is the next greater integer than the oraginal integer.
+# loop from the end of the list, comepair to the ele in the front. 
+
+# list all the permutations and sort to find the next greater one.
+# but, time and space are not efficient 
+
+#
