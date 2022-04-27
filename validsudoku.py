ea3e85b0f6ac6d3692166822ac8b841e3e1e6e42
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-04-27 22:57:25 +0000

    update

--- validsudoku.py
+++ validsudoku.py
@@ -1,2 +1,22 @@
 # https://leetcode.com/problems/valid-sudoku/
 # check if the given soduku is valid or not
+# does not have to be a valid fillable sudoku
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        cols = collections.defaultdict(set)
+        rows = collections.defaultdict(set)
+        # key = (r //3,c //3)
+        squares = collections.defaultdict(set)
+        
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == '.':
+                    continue
+                if (board[r][c] in rows[r] or
+                    board[r][c] in cols[c] or
+                    board[r][c] in squares[(r//3,c//3)]):
+                    return False
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r//3,c//3)].add(board[r][c])
+        return True
