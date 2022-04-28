74f36e9a1e22c39908c32dbc6319c9577bff9cbf
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-04-28 22:41:01 +0000

    update

--- sudokusolver.py
+++ sudokusolver.py
@@ -1,2 +1,15 @@
 # https://leetcode.com/problems/sudoku-solver/
 # backtracking 
+def fillempty(r,c,rows,cols,boxes,board):
+	for r in range(len(rows)):
+		for c in range(len(cols)):
+			if board[r][c] != '.':
+				continue
+	
+			for num in range(1,10):
+				if (num in rows[r] or num in cols[c]
+					or num in boxes[(r//3,c//3)]):
+						continue
+				board[r][c] = num
+				
+				
