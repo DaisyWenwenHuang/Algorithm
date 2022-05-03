4b4d44f68d0ed5b0b6ed82f91627627b32a253d7
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-05-03 13:55:31 +0000

    done

--- permutations.py
+++ permutations.py
@@ -1,1 +1,19 @@
 # https://leetcode.com/problems/permutations/
+Class Solution:
+	def permute(self,nums):
+		res = []
+
+		# base case
+		if len(nums) == 1:
+			# result is a list of list
+			return [nums[:]]
+
+		for i in range(len(nums)):
+			n = nums.pop(0)
+
+			perms = self.permute(nums)
+			for perm in perms:
+				perm.append(n)
+			res.extend(perms)
+			nums.append(n)
+	return res
