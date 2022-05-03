509da138185ef3e475d1c5c689c8a76d029f4cec
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-05-03 22:37:04 +0000

    update

---
+++ permutationii.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/permutations-ii/
+# foolow up  of th permutation problem. the difference is this one 
+# might contain depulicate numbers
+# aka : result need to filter out the depulicate ones.
+# permutation code update
+# need a hashmap to track results
--- wordsearch.py
+++ wordsearch.py
@@ -1,1 +1,34 @@
 # https://leetcode.com/problems/word-search/
+# can not revisit the path, need a set to track the path
+board = [["C","A","A"],["A","A","A"],["B","C","D"]]
+word = 'AAB'
+board1 = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
+word1 = "ABCCED"
+
+rows, cols = len(board1),len(board1[0])
+path = set()
+
+
+def dfs(r,c,i):
+	if i == len(word):
+		return True
+	if (r < 0 or c > 0 or r>= rows or c >= cols 
+	   or word[i] != board[r][c] or (r,c) in path):
+		   return False
+	path.add((r,c))
+	print(path)
+	res = (dfs(r+1,c,i+1) or
+		   dfs(r-1,c, i+1) or
+		   dfs(r, c+1,i+1) or 
+		  dfs(r,c-1,i+1))
+	# remove used path after one path of search
+	path.remove((r,c))
+	print(res)
+	return res
+
+
+for r in range(rows):
+	for c in range(cols):
+		if dfs(r,c,0):
+			print(True)
+print(False)  
