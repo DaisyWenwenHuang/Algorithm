ff73d276e4d5f08869c3bf6574e0ce4207869baf
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-05-06 17:40:13 +0000

    done

--- lettercombphone.py
+++ lettercombphone.py
@@ -1,1 +1,18 @@
 # https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        phone = {'2':'abc','3':'def','4':'ghi','5':'jkl','6':'mno',
+                 '7':'pqrs','8':'tuv','9':'wxyz'}
+        res = []
+
+        def dsf(i,curstr):
+            if len(curstr) == len(digits):
+                res.append(curstr)
+                return
+            
+            for char in phone[digits[i]]:
+                dsf(i + 1,curstr + char)
+		# code below to make sure if the digits is empty, we return [], not ['']
+        if digits:
+            dsf(0,'')
+        return res
