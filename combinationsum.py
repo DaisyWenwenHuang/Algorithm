d8cc53be52c1715e5d1b3d8f08054547c3465100
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-05-09 18:25:39 +0000

    done

--- combinationsum.py
+++ combinationsum.py
@@ -1,2 +1,26 @@
 # https://leetcode.com/problems/combination-sum/
 # backtracking
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        
+        def dsf(i,curcomb,cursum):
+            # base case
+            if cursum == target:
+                res.append(curcomb.copy())
+                return
+            if cursum > target or i >= len(candidates):
+                return
+            
+            # main function
+            # first decision: including [i]
+            curcomb.append(candidates[i])
+            dsf(i,curcomb,cursum + candidates[i])
+            
+            curcomb.pop()
+            # second decision : not including the [i]
+            dsf(i+1,curcomb,cursum)
+            
+        
+        dsf(0,[],0)
+        return  res
