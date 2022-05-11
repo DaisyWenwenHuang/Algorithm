e156183c09d0199b0c2b3ae02711699febd8780c
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-05-11 18:16:21 +0000

    done

--- combinations.py
+++ combinations.py
@@ -1,2 +1,21 @@
 # https://leetcode.com/problems/combinations/
 # backtracking
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+                
+
+        def dfs(start,cur):
+            # base case
+            if len(cur) == k :
+                res.append(cur.copy())
+                return
+            
+            for i in range(start,n + 1):
+                cur.append(i)
+                dfs(i+1, cur)
+            
+                cur.pop()
+            
+        dfs(1,[])
+        return res
