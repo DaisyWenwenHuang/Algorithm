62e054c0e03eeba3e3f16ff044bd8f33fae9c19c
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-05-20 18:16:54 +0000

    update

--- matchstickstosquare.py
+++ matchstickstosquare.py
@@ -37,3 +37,27 @@ class Solution:
         return dfs(4,0,0)
 
 # another solution . accepted
+class Solution:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        length = sum(matchsticks) // 4
+        sides = [0] * 4
+        
+        if sum(matchsticks) / 4 != length:
+            return False
+        
+        matchsticks.sort(reverse=True)
+        
+        def dfs(i):
+            # base case
+            if i == len(matchsticks):
+                return True
+            
+            for j in range(4):
+                if sides[j] + matchsticks[i] <= length:
+                    sides[j] += matchsticks[i]
+                    if dfs(i+1):
+                        return True
+                    sides[j] -= matchsticks[i]
+            return False
+        return dfs(0)
+            
