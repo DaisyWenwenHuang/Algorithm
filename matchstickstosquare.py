cdbb3d77a9ef783f4d63b6392520344982b164fc
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-05-20 02:14:32 +0000

    update

--- matchstickstosquare.py
+++ matchstickstosquare.py
@@ -1,8 +1,9 @@
 # https://leetcode.com/problems/matchsticks-to-square/
 # backtracking 
 # similar to leetcode698
+# example one
 # [5,5,5,5,4,4,4,4,3,3,3,3]
-# 
+# example two
 # [1,1,2,2,2]
 #
                 
