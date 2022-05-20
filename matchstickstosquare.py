daf0cec9d8d676d453c4d1f3ab4e4df5804bbc66
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-05-20 02:15:29 +0000

    update

--- matchstickstosquare.py
+++ matchstickstosquare.py
@@ -6,4 +6,33 @@
 # example two
 # [1,1,2,2,2]
 #
+class Solution:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        k = 4
+        total = sum(matchsticks)
+        used = [False]*len(matchsticks)
+        if total % 4 != 0:
+            return False
+        target = total // 4
+        matchsticks.srot(reverse=True)
+        
+        def dfs(k,cursum,start):
+            # base case
+            if k == 0:
+                return True
+
+            while start < len(matchsticks):
+                cursum += matchsticks[start]
+                if cursum == target and not used[start]:
+                    used[start] = True
+                    return dfs(k-1,0,start+1)
+                if cursum < target:
+                    return dfs(k,cursum,start+1)
                 
+                if cursum > target:
+                    used[start] = False
+                    break
+            return False
+        
+        return dfs(4,0,0)
+                                
