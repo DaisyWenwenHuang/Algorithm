ff14bdb54b01cf70435db01ed26c39c503d478cb
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-05-23 22:14:48 +0000

    update

--- subsets2.py
+++ subsets2.py
@@ -1,2 +1,22 @@
 # https://leetcode.com/problems/subsets-ii/
 # backtracking
+   res = []
+        nums.sort()
+        
+        def backtrack(i, subset):
+            if i == len(nums):
+                res.append(subset[::])
+                return 
+            
+            # all subsets that include nums[i]
+            subset.append(nums[i])
+            backtrack(i+1,subset)
+            
+            subset.pop()
+            # all subsets that dont include nums[i]
+            while i+1 < len(nums) and nums[i] == nums[i+1]:
+                i += 1
+            backtrack(i + 1, subset)
+            
+        backtrack(0,[])
+        return res
