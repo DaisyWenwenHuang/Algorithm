98eea43a8b8d7a1d43653bf3ed2a2c9935c002aa
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-07-30 19:35:37 +0000

    update

--- BST/maxdepthbt.py
+++ BST/maxdepthbt.py
@@ -16,3 +16,20 @@ class Solution:
 
             return 1 + max(dfs(root.left),dfs(root.right))
         return dfs(root)
+
+# solution 2 bfs
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        level = 0
+        q = deque([root])
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level += 1
+        return level
