b68f5542753446d9d22863918f1783591afda279
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-08-01 22:34:25 +0000

    done

---
+++ graphs/clonegraph.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/clone-graph/
+# leetcode 133 medium
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node:
+            return None
+        
+        hashmap = {}
+        
+        def dfs(node):
+            if node in hashmap:
+                return hashmap[node]
+            
+            copy = Node(node.val)
+            hashmap[node] = copy
+            for neib in node.neighbors:
+                copy.neighbors.append(dfs(neib))
+            return copy
+        
+        return dfs(node)
