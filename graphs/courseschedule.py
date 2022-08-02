1336886032ad59d6cd66525ad84676550ffe832a
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-08-02 02:37:19 +0000

    done

--- graphs/courseschedule.py
+++ graphs/courseschedule.py
@@ -1,2 +1,28 @@
 # https://leetcode.com/problems/course-schedule/submissions/
 # leetcode 207 medium
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        premap= {i:[] for i in range(numCourses)}
+        for crs,pre in prerequisites:
+            premap[crs].append(pre)
+            
+        visitset = set()
+        
+        def dfs(crs):
+            if crs in visitset:
+                return False
+            if premap[crs] == []:
+                return True
+            
+            visitset.add(crs)
+            for pre in premap[crs]:
+                if not dfs(pre):
+                    return False
+            visitset.remove(crs)
+            premap[crs] = []
+            return True
+        
+        for crs in range(numCourses):
+            if not dfs(crs):
+                return False
+        return True
