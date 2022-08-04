04c4bd4b5ff21ef3109bcc15de9262e453351777
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-08-04 19:49:57 +0000

    done

--- graphs/rottingoranges.py
+++ graphs/rottingoranges.py
@@ -1,2 +1,27 @@
 # https://leetcode.com/problems/rotting-oranges/
 # leetcode 994 medium
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        q = deque()
+        time,fresh = 0,0
+        rows,cols = len(grid),len(grid[0])
+        
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 2:
+                    q.append([r,c])
+                if grid[r][c] == 1:
+                    fresh += 1
+        directions = [[0,1],[0,-1],[1,0],[-1,0]]
+        while q and fresh > 0:
+            for i in range(len(q)):
+                r,c = q.popleft()
+                for dr,dc in directions:
+                    row,col = dr+r,dc+c
+                    if (row not in range(rows)) or (col not in range(cols)) or grid[row][col] != 1:
+                        continue
+                    grid[row][col] = 2
+                    q.append([row,col])
+                    fresh -= 1
+            time += 1
+        return time if fresh == 0 else -1
