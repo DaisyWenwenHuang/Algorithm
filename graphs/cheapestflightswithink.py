615bdfbf57dbf51f1961227706a883323fdfbf73
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-08-05 23:16:33 +0000

    code

--- graphs/cheapestflightswithink.py
+++ graphs/cheapestflightswithink.py
@@ -1,3 +1,19 @@
 # https://leetcode.com/problems/cheapest-flights-within-k-stops/
 # leetcode 787 medium
 # Bellman-ford algorithm
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        prices = [float('inf')] *n
+        prices[src] = 0
+        
+        for i in range(k+1):
+            tempprices = prices.copy()
+            
+            for s,d,p in flights:
+                if prices[s] == float('inf'):
+                    continue
+                if prices[s] + p < tempprices[d]:
+                    tempprices[d] = prices[s] + p
+            prices = tempprices
+        return -1 if prices[dst] == float('inf') else prices[dst]
+        
