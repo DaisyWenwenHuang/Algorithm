d75f3009eecb6e0e16c933abb2aa08c7ac0e0cd0
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-08-22 18:53:07 +0000

    update

--- graphs/rottenoranges.py
+++ graphs/rottenoranges.py
@@ -2,3 +2,35 @@
 # leetcode 994 medium
 # bfs
 # review
+# 
+        rows = len(grid)
+        cols = len(grid[0])
+        Q = deque()
+        time, fresh = 0, 0
+        directions = [[0,1],[0,-1],[1,0],[-1,0]]
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == 1:
+                    fresh += 1
+                if grid[row][col] == 2:
+                    Q.append([row,col])
+                
+        while Q and fresh > 0:
+            
+            for i in range(len(Q)):
+                r,c = Q.popleft()
+                for dr,dc in directions:
+                    row = r + dr
+                    col = c + dc
+                    
+                    if (row not in range(rows)) or (col not in range(cols)) or grid[row][col] != 1:
+                        continue
+                    grid[row][col] = 2
+                    Q.append([row,col])
+                    fresh -= 1
+            time += 1
+        
+        if fresh == 0:
+            return time
+        else:
+            return -1
