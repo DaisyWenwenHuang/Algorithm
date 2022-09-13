59e1628ebf4564d3de3d1443845cd3ac3c5f1563
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-09-13 16:21:32 +0000

    update

--- dp/combinationsum.py
+++ dp/combinationsum.py
@@ -1,3 +1,23 @@
 # https://leetcode.com/problems/combination-sum/
 # leetcode 39
 # medium
+# return unique combinations 
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        i,j = 0,0
+        
+        def dfs(i,cur,total):
+            if total == target:
+                res.append(cur.copy())
+                return 
+            if i >=len(candidates) or total > target:
+                return
+            
+            cur.append(candidates[i])
+            dfs(i,cur,total+candidates[i])
+            cur.pop()
+            dfs(i+1,cur, total)
+            
+        dfs(0,[],0)
+        return res
