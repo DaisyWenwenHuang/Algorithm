c4b8fb933d16ed1ea2e4bd338a3b1c16a6cdd6e5
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-09-14 16:25:17 +0000

    update

--- dp/climbingstaris.py
+++ dp/climbingstaris.py
@@ -1,3 +1,17 @@
 # https://leetcode.com/problems/climbing-stairs/
 # leetcode 70
 # easy
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        f1 = 1
+        f2 = 1
+        f = 0
+        if n <= 1:
+            return 1
+        for i in range(n-2,-1,-1):
+            f = f1 + f2
+            f2 = f1
+            f1 = f
+        return f
+            
