43943ba2bf67bc651aa8c122dd410a3573caadcd
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-09-14 16:30:15 +0000

    done

--- dp/climbingstaris.py
+++ dp/climbingstaris.py
@@ -16,4 +16,13 @@ class Solution:
             f2 = f1
             f1 = f
         return f
-            
+
+# better code
+class Solution:
+    def climbStairs(self, n: int) -> int:
+		one,two = 1,1
+		for i in range(n-1):
+			temp = one
+			one = one + two
+			two = temp
+		return one 
