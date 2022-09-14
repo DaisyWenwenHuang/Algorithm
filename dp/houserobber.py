763da2e73db7459223a7815ae13e146576dc5d1d
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-09-14 17:55:06 +0000

    done

--- dp/houserobber.py
+++ dp/houserobber.py
@@ -1,3 +1,12 @@
 # https://leetcode.com/problems/house-robber/
 # leetcode 198
 # medium
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        rob1, rob2 = 0,0
+        # [rob1,rob2,n,n+1..]
+        for n in nums:
+            temp = max(rob1+n,rob2)
+            rob1 = rob2
+            rob2 = temp
+        return rob2
