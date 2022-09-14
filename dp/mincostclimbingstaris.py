1b2e1c1e3483c97103ee4cb0f7056c2fec5190e2
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-09-14 17:29:16 +0000

    done

--- dp/mincostclimbingstaris.py
+++ dp/mincostclimbingstaris.py
@@ -1,3 +1,12 @@
 # https://leetcode.com/problems/min-cost-climbing-stairs/
 # leetcode 746
 # easy
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        cost.append(0)
+        
+        for i in range(len(cost)-3,-1,-1):
+            cost[i] += min(cost[i+1],cost[i+2])
+			# return the min of [0] and [1] is because we can choose start from
+			# index 0 or index 1
+        return min(cost[0],cost[1])
