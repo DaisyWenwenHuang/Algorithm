55b048420c91b30dd78c76d9e06a621bac6037ce
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-09-20 18:17:29 +0000

    commnets

--- dp/maxsubarray.py
+++ dp/maxsubarray.py
@@ -4,9 +4,11 @@
 # greedy
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+		# maxsum can not be 0 as there is negative numbers in the list
         maxsum = nums[0]
         cursum = 0
         for n in nums:
+			# whenever the prefix is negative, reset cursum to 0
             if cursum <0 :
                 cursum = 0
             cursum += n
