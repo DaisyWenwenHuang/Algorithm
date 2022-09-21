da1114ab7336790f415c86f3e47a5bdc9f2330a1
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-09-21 22:26:47 +0000

    code

--- dp/jumpgame.py
+++ dp/jumpgame.py
@@ -2,3 +2,14 @@
 # leetcode 55
 # medium
 # greedy
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        flag = len(nums)-1
+        
+        for i in range(len(nums)-1,-1,-1):
+            if i + nums[i] >= flag:
+                flag = i
+        if flag == 0:
+            return True
+        else:
+            return False
