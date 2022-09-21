1ddac6765394dd56a8315ddf05b326ffeb2c286d
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-09-21 23:02:24 +0000

    comments

--- dp/gasstation.py
+++ dp/gasstation.py
@@ -6,8 +6,10 @@ class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         diff = [(i-j) for i,j in zip(gas,cost)]
         if sum(diff) < 0:
+			# no solution
             return -1
         else:
+			# has a unique solution
             total = 0
             start = 0
             for k in range(len(diff)):
@@ -15,5 +17,4 @@ class Solution:
                 if total < 0:
                     total = 0
                     start = k + 1
-                    continue
             return start
