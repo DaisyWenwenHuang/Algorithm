eeeb59a9bdbe343d62dbffcb97795af0027554a6
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-09-21 23:00:23 +0000

    code

--- dp/gasstation.py
+++ dp/gasstation.py
@@ -1,3 +1,19 @@
 # https://leetcode.com/problems/gas-station/
 # leetcode 134
-# 
+# medium
+# greedy
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        diff = [(i-j) for i,j in zip(gas,cost)]
+        if sum(diff) < 0:
+            return -1
+        else:
+            total = 0
+            start = 0
+            for k in range(len(diff)):
+                total += diff[k]
+                if total < 0:
+                    total = 0
+                    start = k + 1
+                    continue
+            return start
