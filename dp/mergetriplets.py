d1e01d22cc66b0a2e95720f39e00366deed7a16d
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-09-28 18:29:55 +0000

    code

--- dp/mergetriplets.py
+++ dp/mergetriplets.py
@@ -5,5 +5,16 @@
 # using zip to avoid two loops 
 class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        t = [0,0,0]
+        
         for triplet in triplets:
-            [x-y for x, y in zip(triplet,target)]
+            if triplet[0] > target[0] or triplet[1]>target[1] or triplet[2]>target[2]:
+                continue
+            for i in range(3):
+                t[i] = max(t[i],triplet[i])
+        
+        for i in range(3):
+            if target[i] != t[i]:
+                return False
+        return True
+                
