dffd522729809026d25e230d1ce45659935b6846
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-09-29 20:49:29 +0000

    code

--- dp/patitionlabels.py
+++ dp/patitionlabels.py
@@ -2,3 +2,18 @@
 # leetcode 763
 # medium
 # greedy
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        counts = {}
+        for index,ele in enumerate(s):
+            counts[ele] = index
+        res = []
+        size = 0
+        end = 0
+        for index, ele in enumerate(s):
+            size += 1
+            end = max(end, counts[ele])
+            if index  == end:
+                res.append(size)
+                size = 0
+        return res
