b777ae2a0b1b2187878d7f9dee59cc4c8523b539
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-09-29 21:57:23 +0000

    code

--- dp/validparenthesisstring.py
+++ dp/validparenthesisstring.py
@@ -2,3 +2,21 @@
 # leetcode 678
 # medium
 # greedy
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        leftmax,leftmin = 0,0
+        for ele in s:
+            if ele == '(':
+                leftmax += 1
+                leftmin += 1
+            elif ele == ")":
+                leftmax -= 1
+                leftmin -= 1
+            else:
+                leftmax += 1
+                leftmin -= 1
+            if leftmax < 0:
+                return False
+            if leftmin < 0:
+                leftmin = 0
+        return leftmin == 0
