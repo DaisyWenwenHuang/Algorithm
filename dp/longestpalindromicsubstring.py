8e003096378a322ed9ae4f4a23b717f2eefaa821
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-09-30 18:53:37 +0000

    code

--- dp/longestpalindromicsubstring.py
+++ dp/longestpalindromicsubstring.py
@@ -1,3 +1,25 @@
 # https://leetcode.com/problems/longest-palindromic-substring/
 # leetcode 5
 # medium
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        res = ''
+        size = 0
+        for i in range(len(s)):
+            left, right = i,i
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                if right - left + 1 > size:
+                    res = s[left:right+1]
+                    size = right-left+1
+                left -= 1
+                right += 1
+            
+            left,right = i,i+1
+            while left >=0 and right<len(s) and s[left]==s[right]:
+                if (right-left+1) > size:
+                    res = s[left:right+1]
+                    size = right-left+1
+                left -= 1
+                right += 1
+
+        return res
