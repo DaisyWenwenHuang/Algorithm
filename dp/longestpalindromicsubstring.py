5cf55859a62efa683b140984f471c4cb92724d2b
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-09-30 18:54:42 +0000

    comments

--- dp/longestpalindromicsubstring.py
+++ dp/longestpalindromicsubstring.py
@@ -6,6 +6,7 @@ class Solution:
         res = ''
         size = 0
         for i in range(len(s)):
+			# odd length 'abac'--> 'aba'
             left, right = i,i
             while left >= 0 and right < len(s) and s[left] == s[right]:
                 if right - left + 1 > size:
@@ -13,7 +14,8 @@ class Solution:
                     size = right-left+1
                 left -= 1
                 right += 1
-            
+
+			# even lenght "abbc" --> 'bb'
             left,right = i,i+1
             while left >=0 and right<len(s) and s[left]==s[right]:
                 if (right-left+1) > size:
