78669948f56a5daf75fb994e64852f36a0850ef7
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-10-03 18:44:17 +0000

    code

--- dp/palindromicsubs.py
+++ dp/palindromicsubs.py
@@ -2,3 +2,26 @@
 # leetcode 647
 # medium 
 # 1-D dp
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        res = len(s)
+        for i in range(len(s)):
+            # odd 
+			# odd length of the palindrom
+            left  = i-1
+            right = i+1
+            while left >= 0 and right < len(s) and s[left]==s[right]:
+                res += 1
+                left -= 1
+                right += 1
+            
+            # even 
+			# even length of the palindrom
+            left = i
+            right = i+1
+            while left >=0 and right < len(s) and s[left] == s[right]:
+                res += 1
+                left -=1
+                right += 1
+        return res
+	
