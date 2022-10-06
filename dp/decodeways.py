267649d620e8c03930a3086ec10c7184b6948dd6
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-10-06 21:57:19 +0000

    update

--- dp/decodeways.py
+++ dp/decodeways.py
@@ -2,3 +2,9 @@
 # leetcode 91 
 # medium
 # 1-d dp
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        dp = [0]*len(s)
+        for i in range(len(s)-1,-1,-1):
+            # encounter 0
+            if s[i] == 0
